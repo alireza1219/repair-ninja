f9135b57039e41ede1e30ae0ba45fe826f507d22
Alireza Barani <baranali02@gmail.com>
2024-05-06 19:22:04 +0330

    Extending the Djsoer's UserSerializer. Include the authenticated user access level and permissions in user profile.

--- repair_ninja/serializers.py
+++ repair_ninja/serializers.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import get_user_model
 from djoser.serializers import UserSerializer as BaseUserSerializer
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from rest_framework import serializers
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
@@ -9,5 +11,25 @@ class UserCreateSerializer(BaseUserCreateSerializer):
 
 
 class UserSerializer(BaseUserSerializer):
+    permissions = serializers.SerializerMethodField(read_only=True)
+    type = serializers.SerializerMethodField(read_only=True)
+
     class Meta(BaseUserSerializer.Meta):
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email',
+                  'first_name', 'last_name', 'type', 'permissions']
+    
+    def get_type(self, obj):
+        if (obj.is_superuser):
+            return "superuser"
+        elif (obj.is_staff):
+            return "staff"
+        return "regular"
+
+    def get_permissions(self, obj):
+        User = get_user_model()
+        all_permissions = User(is_superuser=True).get_all_permissions()
+        # We're only interested in repair_core app permissions.
+        repair_core_permissions = filter(
+            lambda x: x.startswith('repair_core'), all_permissions)
+        user_permissions = obj.get_all_permissions()
+        return {p: p in user_permissions for p in repair_core_permissions}
